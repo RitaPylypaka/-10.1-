f126ccb2b969f26c799570b51b30410cd9214208
RitaPylypaka <ritapylypaka@gmail.com>
2023-12-06 22:15:34 +0200

    Add project files.

---
+++ UnitTest10.1C++/UnitTest10.1C++.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest101C
+{
+	TEST_CLASS(UnitTest101C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
